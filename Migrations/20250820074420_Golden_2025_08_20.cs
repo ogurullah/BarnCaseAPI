b7c7a0aa78be50877974feda6638d30988a8d403
ogurullah <oguzkagancelik16@gmail.com>
2025-08-20 10:44:20 +0300

    DB snapshot: Golden_2025_08_20

---
+++ Migrations/20250820074420_Golden_2025_08_20.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BarnCaseAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Golden_2025_08_20 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
